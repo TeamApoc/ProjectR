4c4d01ac2639187d7528bd023a36b917d6f0c505
baeyc0510 <baeyc0510@gmail.com>
2026-05-14 15:45:53 +0900

    feat: 동료 소생 어빌리티 추가 (#79)
    ## 개요
    
    - 다운 상태 동료를 홀드 상호작용으로 소생시키는 흐름을 추가
    - 상호작용 액션이 게임플레이 이벤트로 시술자/대상의 GA를 분리 트리거하도록 구성
    
    ## 주요 변경사항
    
    ### 소생 상호작용 - `UPRInteraction_Revive`
    
    홀드 상호작용 액션으로 구현. `CanInteract`은 대상(GetOwner) ASC에 `State_Down` 태그가 있을 때만
    통과. 홀드 시작 시 시술자(Interactor) ASC에 `Event.Ability.Revive` 이벤트를 보내
    GA_Revive 진입, 홀드 취소/중단 시 시술자 ASC의 `Ability.Player.Revive` 태그 GA를
    `CancelAbilities`로 정리. 홀드 완료(`Execute`) 시 대상 ASC에 `Event.Ability.GetUp`
    이벤트를 보내 다운 GA가 기상 흐름으로 전환되도록 처리.
    
    ### GameplayTag 추가 - `PRGameplayTags`
    
    - `Ability.Player.Revive` - 시술자 소생 GA 식별 태그
    - `Event.Ability.Revive` - 시술자 GA 활성화 이벤트
    - (기존 `Event_Ability_GetUP` 오타 동시 정정)
    
    ### 에셋
    
    - `GA_Revive`, `GA_GetUp` 어빌리티, `BP_Interaction_Revive` 상호작용 액션, 소생/기상
    몽타주(`AM_Player_M_UN_Action_Revive*`, `AM_Player_Wounded_GetUp`) 추가,
    `DA_PlayerAbilitySet`/`BP_PlayerCharacter` 갱신

---
+++ Source/ProjectR/Interaction/Actions/PRInteraction_Revive.cpp
@@ -0,0 +1,60 @@
+﻿// Copyright (c) 2026 TeamApoc. All Rights Reserved.
+
+
+#include "PRInteraction_Revive.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "ProjectR/PRGameplayTags.h"
+#include "ProjectR/Utils/PRGameplayStatics.h"
+
+void UPRInteraction_Revive::OnHoldStart_Implementation(AActor* Interactor)
+{
+	Super::OnHoldStart_Implementation(Interactor);
+	
+	if (UAbilitySystemComponent* ASC = UPRGameplayStatics::GetAbilitySystemComponent(Interactor))
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(ASC->GetOwner(),PRGameplayTags::Event_Ability_Revive,FGameplayEventData());
+	}
+}
+
+void UPRInteraction_Revive::OnHoldCanceled_Implementation(AActor* Interactor)
+{
+	Super::OnHoldCanceled_Implementation(Interactor);
+	FinishReviveAbility(Interactor);
+}
+
+void UPRInteraction_Revive::OnHoldFinished_Implementation(AActor* Interactor)
+{
+	Super::OnHoldFinished_Implementation(Interactor);
+	FinishReviveAbility(Interactor);
+}
+
+void UPRInteraction_Revive::Execute_Implementation(AActor* Interactor)
+{
+	Super::Execute_Implementation(Interactor);
+	
+	if (UAbilitySystemComponent* ASC = UPRGameplayStatics::GetAbilitySystemComponent(GetOwner()))
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(ASC->GetOwner(),PRGameplayTags::Event_Ability_GetUp,FGameplayEventData());
+	}
+}
+
+bool UPRInteraction_Revive::CanInteract_Implementation(AActor* Interactor) const
+{
+	if (UAbilitySystemComponent* ASC = UPRGameplayStatics::GetAbilitySystemComponent(GetOwner()))
+	{
+		return ASC->HasMatchingGameplayTag(PRGameplayTags::State_Down);
+	}
+	return false;
+}
+
+void UPRInteraction_Revive::FinishReviveAbility(AActor* Interactor)
+{
+	if (UAbilitySystemComponent* ASC = UPRGameplayStatics::GetAbilitySystemComponent(Interactor))
+	{
+		FGameplayTagContainer TagContainer;
+		TagContainer.AddTag(PRGameplayTags::Ability_Player_Revive);
+		ASC->CancelAbilities(&TagContainer);
+	}
+}
---
+++ Source/ProjectR/Interaction/Actions/PRInteraction_Revive.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2026 TeamApoc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectR/Interaction/PRInteractionAction.h"
+#include "PRInteraction_Revive.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UPRInteraction_Revive : public UPRInteractionAction
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnHoldStart_Implementation(AActor* Interactor) override;
+	virtual void OnHoldCanceled_Implementation(AActor* Interactor) override;
+	virtual void OnHoldFinished_Implementation(AActor* Interactor) override;
+	virtual void Execute_Implementation(AActor* Interactor) override;
+	virtual bool CanInteract_Implementation(AActor* Interactor) const override;
+	
+private:
+	void FinishReviveAbility(AActor* Interactor);
+};

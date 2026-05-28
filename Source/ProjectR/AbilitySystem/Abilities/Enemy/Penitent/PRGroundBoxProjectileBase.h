957d234b5d7b0f50cfca90931fc8d1557dc2bb68
Ymir <ymir789@gmail.com>
2026-05-28 18:03:31 +0900

    feat: GroundBoxProjectileBase 추가

---
+++ Source/ProjectR/AbilitySystem/Abilities/Enemy/Penitent/PRGroundBoxProjectileBase.h
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2026 TeamApoc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PRGroundBoxProjectileBase.generated.h"
+
+UCLASS()
+class PROJECTR_API APRGroundBoxProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APRGroundBoxProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
